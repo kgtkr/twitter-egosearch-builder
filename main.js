66011b6ad95dfdbcaa76923c3dc616483a09ebd7
kgtkr <kgtkr.jp@gmail.com>
2018-11-12 21:14:02 +0900

    完成

---
+++ main.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+
+const config = JSON.parse(fs.readFileSync("config.json", { encoding: "utf8" }));
+fs.writeFileSync("output.txt", config.include_words.join(" OR ") + " " + config.self_accounts.map(x => `-from:${x} -@${x}`).join(" ") + " " + config.exclude_accounts.map(x => `-@${x}`).join(" "));
